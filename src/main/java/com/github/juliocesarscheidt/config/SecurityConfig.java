01a40edfa8cc0b03dc695d2de531f98668273320
juliocesarscheidt <julio.scheidt96@gmail.com>
2021-07-23 02:20:04 -0300

    Fix config

--- src/main/java/com/github/juliocesarscheidt/config/SecurityConfig.java
+++ src/main/java/com/github/juliocesarscheidt/config/SecurityConfig.java
@@ -15,33 +15,33 @@ import com.github.juliocesarscheidt.security.jwt.JwtTokenProvider;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private JwtTokenProvider jwtTokenProvider;
+  @Autowired
+  private JwtTokenProvider jwtTokenProvider;
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		// creates a encoder for bcrypt passwords
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    // creates a encoder for bcrypt passwords
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-		return encoder;
-	}
+    return encoder;
+  }
 
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.httpBasic().disable()
-			.csrf().disable()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-				.authorizeRequests()
-				.antMatchers("/v1/auth/**", "/actuator/**", "/v2/api-docs/**", "/swagger-ui/**").permitAll()
-				.antMatchers("/v1/customer/**", "/v1/book/**").authenticated()
-			.and()
-			.apply(new JwtConfigurer(jwtTokenProvider));
-	}
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .httpBasic().disable()
+      .csrf().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
+        .authorizeRequests()
+        .antMatchers("/v1/auth/**", "/actuator/**", "/v2/api-docs/**", "/swagger-ui/**").permitAll()
+        .antMatchers("/v1/customer/**", "/v1/book/**").authenticated()
+      .and()
+      .apply(new JwtConfigurer(jwtTokenProvider));
+  }
 }
